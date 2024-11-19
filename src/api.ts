86f88da39162261f795cd25e00a84a13cd253643
samwondim <samwondim@gmail.com>
2024-11-19 17:07:39 +0300

    feat: add post and get endpoints for teachers. Now we can create teachers and query all of them from the db

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,33 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
+const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
 app.get('/health', (req, res) => {
   res.status(200).send({ status: 'API is running!' });
+});
+
+app.get('/teachers', async (req, res) => {
+  const users = await prisma.teacher.findMany();
+  res.json(users);
+});
+
+app.get('/teachers/:phone_number', async (req, res) => {
+
+});
+
+app.post('/teacher/create', async (req, res) => {
+  const { name, phone_number } = req.body;
+  const result = await prisma.teacher.create({
+    data: {
+      name,
+      phone_number
+    }
+  });
+  res.json(result);
 });
 
 export const startAPI = () => {
