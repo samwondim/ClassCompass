284aff6f3a880a86777b77acafd5146150f53fe1
samwondim <samwondim@gmail.com>
2024-11-19 16:28:10 +0300

    chore: cleaned up the index.ts file to import neccessary files from api and bot logic holding files

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
-import { Telegraf, Context, session } from 'telegraf';
+import { startAPI } from "./api";
+import { startBot } from "./bot";
+
 import * as dotenv from "dotenv";
 
-interface SessionData {
-  customProp: number;
-}
-
-interface MyContext extends Context {
-  session?: SessionData;
-}
+//load the config files from .env
 dotenv.config();
-// Replace 'YOUR_BOT_TOKEN' with your bot's token from BotFather
-const bot = new Telegraf<MyContext>(process.env.BOT_TOKEN as string);
-bot.use(session());
 
-// Start command
-bot.start((ctx) => ctx.reply('Hello! Welcome to my Telegram bot!'));
-
-// Echo back any text message sent by users
-bot.on('message', async (ctx) => {
-  ctx.session ??= { customProp: 1 }
-  await ctx.reply('' + ctx.session.customProp);
-});
-
-// Launch the bot
-bot.launch()
-  .then(() => console.log('Bot started successfully'))
-  .catch((err) => console.error('Error starting bot:', err));
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
+startBot();
+startAPI();
