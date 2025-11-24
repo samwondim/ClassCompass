c780f2f8d4f921198a88b4fa6f90b01435cccd3b
samwondim <samwondim@gmail.com>
2025-11-24 18:13:29 +0300

    fix fetch url.

--- components/app-layout.tsx
+++ components/app-layout.tsx
@@ -17,7 +17,7 @@ interface AppLayoutProps {
 }
 
 async function logout() {
-  fetch("/api/logout", { method: "GET" });
+  fetch("/auth/logout", { method: "GET" });
 }
 
 export function AppLayout({ children, userRole }: AppLayoutProps) {
