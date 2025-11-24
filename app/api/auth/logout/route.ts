768037f3a014daacdc408f807559835a24fac6d8
samwondim <samwondim@gmail.com>
2025-11-24 18:11:53 +0300

    fix default export statement.

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -3,8 +3,8 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { NextResponse, NextRequest } from "next/server";
 
-export default async function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const cookieStore = await cookies();
   cookieStore.delete("session");
-  redirect('/login')
+  redirect('/')
 }
