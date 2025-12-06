38937d98268165bfd42f0be1348b075dbdebc4a4
samwondim <samwondim@gmail.com>
2025-12-06 10:02:00 +0300

    restore auth route.

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -7,11 +7,7 @@ import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-    // const { initData } = await request.json();
-    const { initData } = {
-      "initData":
-        "user=%7B%22id%22%3A1845537164%2C%22first_name%22%3A%22Sam%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22triviosa%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FaMUFOe5cu11VbYZNpOC5ziSHBZLfje2U2B-RjvbGd4M.svg%22%7D&chat_instance=-7201700833685701877&chat_type=private&auth_date=1764497960&signature=lL-1KJ1dZVUXcpJEz6n637gA4M1offvHTjE0U8mvBeW-2cdCbEC9d7vlAS3agt4eTNM7XYddvBNtb5spitk2CA&hash=e2fc4177ede877442d64eb168b0cc01bd3c5e46fb073c4df6ca566e9d8ced6e6"
-    };
+    const { initData } = await request.json();
 
     const BOT_TOKEN = process.env.TELEGRAM_API_KEY;
 
